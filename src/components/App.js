34c97a71a3e174902efbbdb5957282e663c36b2b
viveklb <69708672+viveklb@users.noreply.github.com>
2026-06-30 11:07:35 +0000

    ordered list updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+  const cities = [
+    "Mumbai",
+    "Delhi",
+    "Bengaluru",
+    "Hyderabad",
+    "Chennai"
+  ];
 
+  return (
+    <div id="main">
+      <ol>
+        {cities.map((city, index) => (
+          <li key={`location${index + 1}`}>
+            {city}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
 
 export default App;
